c1b22b94f386abdc320f007e5e6647066970266b
Gabe Jensen <thegabejensen@gmail.com>
2023-10-11 19:07:52 -0700

    Got the bar chart race actually changing

--- src/components/page-views/BarChartRacePage.tsx
+++ src/components/page-views/BarChartRacePage.tsx
@@ -2,22 +2,46 @@ import React, { useRef, useEffect, useState } from "react";
 import * as d3 from "d3";
 import data from "../../data/data.json";
 import useEffectOnce from "@/src/hooks/useEffectOnce";
+import Head from "next/head";
+import { delay } from "@/src/utils/delay";
 
+// Number of bars
 const n = 12;
+
+// The amount of "chunks" to break up the year values (we use linear interpolation)
 const k = 10;
-const width = 600;
-const height = 400;
+
+// Duration of how long it takes to swap two rows (in ms)
 const duration = 250;
-const barSize = 48;
+
+// Height of the bar
+const barSize = 20;
 const margin = { top: 16, right: 6, bottom: 6, left: 0 };
+
+const width = 600;
+const height = margin.top + barSize * n + margin.bottom;
+
 const x = d3.scaleLinear([0, 1], [margin.left, width - margin.right]);
+const y = d3
+  .scaleBand()
+  .domain(d3.range(n + 1).map((d) => d.toString()))
+  .rangeRound([margin.top, margin.top + barSize * (n + 1 + 0.1)])
+  .padding(0.1);
+const formatDate = d3.utcFormat("%Y");
+const formatNumber = d3.format(",d");
 
 // [
 //    {date: '2000-01-01T00:00:00.000Z', name: 'Coca-Cola', category: 'Beverages', value: 72537},
 // ]
 
 export default function BarChartRacePage() {
-  useEffectOnce(() => {
+  useEffectOnce(async () => {
+    /*
+     *   datevalues = [
+     *     [Date(), Map(name, value)],
+     *     [Date(), Map(name, value)],
+     *   ]
+     */
     const datevalues = Array.from(
       d3.rollup(
         data,
@@ -28,11 +52,11 @@ export default function BarChartRacePage() {
     )
       .map(([date, data]) => [new Date(date), data])
       .sort(([a], [b]) => d3.ascending(a as Date, b as Date));
-    console.log("\n\n***** datevalues *****\n", datevalues, "\n\n");
 
     const names = new Set(data.map((d) => d.name));
 
-    function rank(value) {
+    // Rank all the companies for one particular day
+    function rank(value: (name: string) => number) {
       const data = Array.from(names, (name) => ({
         name,
         value: value(name),
@@ -47,10 +71,23 @@ export default function BarChartRacePage() {
       return data;
     }
 
+    // Break out every year into smaller "chunks" so that it's easier to follow
+    // and animate. Otherwise several years will change at once and it's hard to
+    // understand what's going on.
     function getKeyframes() {
       const keyframes: any[] = [];
+
       let ka, a, kb, b;
-      for ([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
+
+      /*
+       * pairs = [
+            [ [Date(), Map(name, value)], [Date(), Map(name, value)] ],
+            [ [Date(), Map(name, value)], [Date(), Map(name, value)] ],
+          ]
+       */
+      const pairs = d3.pairs(datevalues);
+
+      for ([[ka, a], [kb, b]] of pairs) {
         for (let i = 0; i < k; ++i) {
           const t = i / k;
           keyframes.push([
@@ -66,191 +103,219 @@ export default function BarChartRacePage() {
       return keyframes;
     }
 
-    // TODO: figure out what's going on in getKeyframes()
     const keyframes = getKeyframes();
 
-    // const nameframes = d3.groups(
-    //   keyframes.flatMap(([, data]) => data),
-    //   (d) => d.name
-    // );
-    // const next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)));
-    // const prev = new Map(
-    //   nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a]))
-    // );
-    // const formatDate = d3.utcFormat("%Y");
-    // const formatNumber = d3.format(",d");
-    // function textTween(a, b) {
-    //   const i = d3.interpolateNumber(a, b);
-    //   return function (t) {
-    //     this.textContent = formatNumber(i(t));
-    //   };
-    // }
-    // const color = () => {
-    //   const scale = d3.scaleOrdinal(d3.schemeTableau10);
-    //   if (data.some((d) => d.category !== undefined)) {
-    //     const categoryByName = new Map(data.map((d) => [d.name, d.category]));
-    //     scale.domain(Array.from(categoryByName.values()));
-    //     return (d) => scale(categoryByName.get(d.name) ?? "");
-    //   }
-    //   return (d) => scale(d.name);
-    // };
+    const nameframes = d3.groups(
+      keyframes.flatMap(([, data]) => data),
+      (d) => d.name
+    );
 
-    // function ticker(svg) {
-    //   const now = svg
-    //     .append("text")
-    //     .style("font", `bold ${barSize}px var(--sans-serif)`)
-    //     .style("font-variant-numeric", "tabular-nums")
-    //     .attr("text-anchor", "end")
-    //     .attr("x", width - 6)
-    //     .attr("y", margin.top + barSize * (n - 0.45))
-    //     .attr("dy", "0.32em")
-    //     .text(formatDate(keyframes[0][0]));
-    //   return ([date], transition) => {
-    //     transition.end().then(() => now.text(formatDate(date)));
-    //   };
-    // }
-    // function axis(svg) {
-    //   const g = svg.append("g").attr("transform", `translate(0,${margin.top})`);
-    //   const axis = d3
-    //     .axisTop(x)
-    //     .ticks(width / 160)
-    //     .tickSizeOuter(0)
-    //     .tickSizeInner(-barSize * (n + y.padding()));
-    //   return (_, transition) => {
-    //     g.transition(transition).call(axis);
-    //     g.select(".tick:first-of-type text").remove();
-    //     g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
-    //     g.select(".domain").remove();
-    //   };
-    // }
-    // function labels(svg) {
-    //   let label = svg
-    //     .append("g")
-    //     .style("font", "bold 12px var(--sans-serif)")
-    //     .style("font-variant-numeric", "tabular-nums")
-    //     .attr("text-anchor", "end")
-    //     .selectAll("text");
-    //   return ([date, data], transition) =>
-    //     (label = label
-    //       .data(data.slice(0, n), (d) => d.name)
-    //       .join(
-    //         (enter) =>
-    //           enter
-    //             .append("text")
-    //             .attr(
-    //               "transform",
-    //               (d) =>
-    //                 `translate(${x((prev.get(d) || d).value)},${y(
-    //                   (prev.get(d) || d).rank
-    //                 )})`
-    //             )
-    //             .attr("y", y.bandwidth() / 2)
-    //             .attr("x", -6)
-    //             .attr("dy", "-0.25em")
-    //             .text((d) => d.name)
-    //             .call((text) =>
-    //               text
-    //                 .append("tspan")
-    //                 .attr("fill-opacity", 0.7)
-    //                 .attr("font-weight", "normal")
-    //                 .attr("x", -6)
-    //                 .attr("dy", "1.15em")
-    //             ),
-    //         (update) => update,
-    //         (exit) =>
-    //           exit
-    //             .transition(transition)
-    //             .remove()
-    //             .attr(
-    //               "transform",
-    //               (d) =>
-    //                 `translate(${x((next.get(d) || d).value)},${y(
-    //                   (next.get(d) || d).rank
-    //                 )})`
-    //             )
-    //             .call((g) =>
-    //               g
-    //                 .select("tspan")
-    //                 .tween("text", (d) =>
-    //                   textTween(d.value, (next.get(d) || d).value)
-    //                 )
-    //             )
-    //       )
-    //       .call((bar) =>
-    //         bar
-    //           .transition(transition)
-    //           .attr("transform", (d) => `translate(${x(d.value)},${y(d.rank)})`)
-    //           .call((g) =>
-    //             g
-    //               .select("tspan")
-    //               .tween("text", (d) =>
-    //                 textTween((prev.get(d) || d).value, d.value)
-    //               )
-    //           )
-    //       ));
-    // }
-    // function bars(svg) {
-    //   let bar = svg.append("g").attr("fill-opacity", 0.6).selectAll("rect");
-    //   return ([date, data], transition) =>
-    //     (bar = bar
-    //       .data(data.slice(0, n), (d) => d.name)
-    //       .join(
-    //         (enter) =>
-    //           enter
-    //             .append("rect")
-    //             .attr("fill", color)
-    //             .attr("height", y.bandwidth())
-    //             .attr("x", x(0))
-    //             .attr("y", (d) => y((prev.get(d) || d).rank))
-    //             .attr("width", (d) => x((prev.get(d) || d).value) - x(0)),
-    //         (update) => update,
-    //         (exit) =>
-    //           exit
-    //             .transition(transition)
-    //             .remove()
-    //             .attr("y", (d) => y((next.get(d) || d).rank))
-    //             .attr("width", (d) => x((next.get(d) || d).value) - x(0))
-    //       )
-    //       .call((bar) =>
-    //         bar
-    //           .transition(transition)
-    //           .attr("y", (d) => y(d.rank))
-    //           .attr("width", (d) => x(d.value) - x(0))
-    //       ));
-    // }
-    // console.log("yess");
-    // const svg = d3
-    //   .select("body")
-    //   .append("svg")
-    //   .attr("viewBox", [0, 0, width, height]);
-    // const updateBars = bars(svg);
-    // const updateAxis = axis(svg);
-    // const updateLabels = labels(svg);
-    // const updateTicker = ticker(svg);
-    // for (const keyframe of keyframes) {
-    //   const transition = svg
-    //     .transition()
-    //     .duration(duration)
-    //     .ease(d3.easeLinear);
-    //   // Extract the top bar’s value.
-    //   x.domain([0, keyframe[1][0].value]);
-    //   updateAxis(keyframe, transition);
-    //   updateBars(keyframe, transition);
-    //   updateLabels(keyframe, transition);
-    //   updateTicker(keyframe, transition);
-    // }
+    const next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)));
+    const prev = new Map(
+      nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a]))
+    );
+
+    // For transitioning text between updates
+    function textTween(a, b) {
+      const i = d3.interpolateNumber(a, b);
+      return function (t) {
+        this.textContent = formatNumber(i(t));
+      };
+    }
+
+    const color = () => {
+      const scale = d3.scaleOrdinal(d3.schemeTableau10);
+
+      if (data.some((d) => d.category !== undefined)) {
+        const categoryByName = new Map(data.map((d) => [d.name, d.category]));
+        scale.domain(Array.from(categoryByName.values()));
+
+        return (d) => {
+          return scale(categoryByName.get(d.name) ?? d.name);
+        };
+      }
+
+      return (d) => {
+        return scale(d.name);
+      };
+    };
+
+    function ticker(svg) {
+      const now = svg
+        .append("text")
+        .style("font", `bold ${barSize}px var(--sans-serif)`)
+        .style("font-variant-numeric", "tabular-nums")
+        .attr("text-anchor", "end")
+        .attr("x", width - 6)
+        .attr("y", margin.top + barSize * (n - 0.45))
+        .attr("dy", "0.32em")
+        .text(formatDate(keyframes[0][0]));
+
+      return ([date], transition) => {
+        transition.end().then(() => now.text(formatDate(date)));
+      };
+    }
+
+    function axis(svg) {
+      const g = svg.append("g").attr("transform", `translate(0,${margin.top})`);
+      const axis = d3
+        .axisTop(x)
+        .ticks(width / 160)
+        .tickSizeOuter(0)
+        .tickSizeInner(-barSize * (n + y.padding()));
+
+      return (_, transition) => {
+        g.transition(transition).call(axis);
+        g.select(".tick:first-of-type text").remove();
+        g.selectAll(".tick:not(:first-of-type) line").attr("stroke", "white");
+        g.select(".domain").remove();
+      };
+    }
+
+    function labels(svg) {
+      let label = svg
+        .append("g")
+        .style("font", "bold 12px var(--sans-serif)")
+        .style("font-variant-numeric", "tabular-nums")
+        .attr("text-anchor", "end")
+        .selectAll("text");
+
+      return ([date, data], transition) =>
+        (label = label
+          .data(data.slice(0, n), (d) => d.name)
+          .join(
+            (enter) =>
+              enter
+                .append("text")
+                .attr(
+                  "transform",
+                  (d) =>
+                    `translate(${x((prev.get(d) || d).value)},${y(
+                      (prev.get(d) || d).rank
+                    )})`
+                )
+                .attr("y", y.bandwidth() / 2)
+                .attr("x", -6)
+                .attr("dy", "-0.25em")
+                .text((d) => d.name)
+                .call((text) =>
+                  text
+                    .append("tspan")
+                    .attr("fill-opacity", 0.7)
+                    .attr("font-weight", "normal")
+                    .attr("x", -6)
+                    .attr("dy", "1.15em")
+                ),
+            (update) => update,
+            (exit) =>
+              exit
+                .transition(transition)
+                .remove()
+                .attr(
+                  "transform",
+                  (d) =>
+                    `translate(${x((next.get(d) || d).value)},${y(
+                      (next.get(d) || d).rank
+                    )})`
+                )
+                .call((g) =>
+                  g
+                    .select("tspan")
+                    .tween("text", (d) =>
+                      textTween(d.value, (next.get(d) || d).value)
+                    )
+                )
+          )
+          .call((bar) =>
+            bar
+              .transition(transition)
+              .attr("transform", (d) => `translate(${x(d.value)},${y(d.rank)})`)
+              .call((g) =>
+                g
+                  .select("tspan")
+                  .tween("text", (d) =>
+                    textTween((prev.get(d) || d).value, d.value)
+                  )
+              )
+          ));
+    }
+
+    function bars(svg) {
+      let bar = svg.append("g").attr("fill-opacity", 0.1).selectAll("rect");
+
+      return ([date, data], transition) =>
+        (bar = bar
+          .data(data.slice(0, n), (d) => d.name)
+          .join(
+            (enter) =>
+              enter
+                .append("rect")
+                .attr("fill", color())
+                .attr("height", y.bandwidth())
+                .attr("x", x(0))
+                .attr("y", (d) => y((prev.get(d) || d).rank))
+                .attr("width", (d) => x((prev.get(d) || d).value) - x(0)),
+            (update) => update,
+            (exit) =>
+              exit
+                .transition(transition)
+                .remove()
+                .attr("y", (d) => y((next.get(d) || d).rank))
+                .attr("width", (d) => x((next.get(d) || d).value) - x(0))
+          )
+          .call((bar) =>
+            bar
+              .transition(transition)
+              .attr("y", (d) => {
+                return y(d.rank);
+              })
+              .attr("width", (d) => x(d.value) - x(0))
+          ));
+    }
+
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("viewBox", [0, 0, width, height]);
+
+    const updateBars = bars(svg);
+    const updateAxis = axis(svg);
+    const updateLabels = labels(svg);
+    const updateTicker = ticker(svg);
+
+    for (const keyframe of keyframes) {
+      const transition = svg
+        .transition()
+        .duration(duration)
+        .ease(d3.easeLinear);
+
+      // Extract the top bar’s value.
+      x.domain([0, keyframe[1][0].value]);
+      updateAxis(keyframe, transition);
+      updateBars(keyframe, transition);
+      // updateLabels(keyframe, transition);
+      // updateTicker(keyframe, transition);
+
+      await delay(100);
+    }
   });
 
   return (
-    <div>
-      <svg
-        width={width + margin.left + margin.right}
-        height={height + margin.top + margin.bottom}
-      >
-        <g transform={`translate(${margin.left},${margin.top})`}>
-          {/* Bars will be rendered here */}
-        </g>
-      </svg>
-    </div>
+    <>
+      <Head>
+        <title>Bar Chart Race</title>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+      </Head>
+      <div>
+        <svg
+          width={width + margin.left + margin.right}
+          height={height + margin.top + margin.bottom}
+        >
+          <g transform={`translate(${margin.left},${margin.top})`}>
+            {/* Bars will be rendered here */}
+          </g>
+        </svg>
+      </div>
+    </>
   );
 }
