e99114df4c99ce946d75a9c47a362cbc9b2c040b
Gabe Jensen <thegabejensen@gmail.com>
2023-11-24 16:31:24 -0800

    Do not do any data pull for ending slide lol

--- src/components/slides/EndingSlide.tsx
+++ src/components/slides/EndingSlide.tsx
@@ -1,35 +1,13 @@
-import { useStats } from "@/src/hooks/endpoints/useStats";
-import { FileCount } from "@/src/types/git";
-import LoadingSpinner from "../LoadingSpinner";
-
 type EndingSlideProps = {
   part: string;
 };
 
 export default function EndingSlide({ part }: EndingSlideProps) {
-  const { data, error, isLoading } = useStats<FileCount>({
-    part: "fileCount",
-  });
-
   return (
     <div className="EndingSlide">
-      {data && (
+      {part === "main" && (
         <div>
-          {part === "main" && (
-            <div>
-              <h1 className="text-7xl slide-fade-in">The End</h1>
-            </div>
-          )}
-        </div>
-      )}
-      {isLoading && (
-        <div>
-          <LoadingSpinner />
-        </div>
-      )}
-      {error && (
-        <div>
-          <div>Error happened!</div>
+          <h1 className="text-7xl slide-fade-in">The End</h1>
         </div>
       )}
     </div>
